6863866b3b12ccf1393142785eb2d476e21f01ca
verry <alfauziveri@gmail.com>
2023-11-18 20:36:43 +0700

    perubahan

---
+++ index.js
@@ -0,0 +1,6 @@
+var nama = confirm('Kamu yakin akan melihat Portfolio Saya??');
+if( nama === true ){
+    alert('Selamat Datang di Portfolio Saya');
+} else{
+    alert('Sampai jumpa lain waktu');
+}
